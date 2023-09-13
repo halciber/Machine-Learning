1b6fcdfbab9de490abb7cfebffc8f133e80cde8c
Mike Goldweber <mikegoldweber@yahoo.com>
2023-09-12 21:31:35 -0400

    Setting Up Project
    Organizing the project's structure and getting some files started.

---
+++ UnsupervisedWisdom/DataExploration/DataExploration.R
@@ -0,0 +1,5 @@
+#DataExploration.R
+
+#Looking at the data to get a feel for it.
+
+summary(df_primary)
---
+++ UnsupervisedWisdom/Misc/ReadData.R
@@ -0,0 +1,8 @@
+#ReadData.R
+#Open data files and place code into a data frame.
+
+df_primary <- read.csv("c:\\_working\\Machine-Learning\\UnsupervisedWisdom\\Data\\primary_data.csv")
+
+# Print the first few rows of the data frame to verify the import
+head(df_primary)
+
